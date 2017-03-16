4f45273333134aee263e7bf0860dfb4c4f45f314
Oscar Martin Garcia <omartin1978@hotmail.com>
2017-03-16 11:31:46 +0100

    ava-properties
    Initial version

---
+++ ava-properties/src/webparts/avanadeProperties/IAvanadePropertiesWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IAvanadePropertiesWebPartProps {
+  description: string;
+}
---
+++ ava-properties/src/webparts/avanadeProperties/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAvanadePropertiesStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'avanadePropertiesStrings' {
+  const strings: IAvanadePropertiesStrings;
+  export = strings;
+}
