1eed5a36cc5afbd0752de2ce326a7280bd48663e
Oscar Martin Garcia <omartin1978@hotmail.com>
2017-03-17 08:00:17 +0100

    ava-log
    Try to show images using webpack file-loader

--- ava-log/src/webparts/avanadeLog/AvanadeLogWebPart.ts
+++ ava-log/src/webparts/avanadeLog/AvanadeLogWebPart.ts
@@ -10,9 +10,16 @@ import styles from './AvanadeLog.module.scss';
 import * as strings from 'avanadeLogStrings';
 import { IAvanadeLogWebPartProps } from './IAvanadeLogWebPartProps';
 
+//OMG - import Log class
+import { Log } from '@microsoft/sp-core-library';
+
 export default class AvanadeLogWebPart extends BaseClientSideWebPart<IAvanadeLogWebPartProps> {
 
   public render(): void {
+    
+    var url = require('file-loader!./log.png');
+    //var url = require("./log.png");
+
     this.domElement.innerHTML = `
       <div class="${styles.helloWorld}">
         <div class="${styles.container}">
@@ -21,13 +28,24 @@ export default class AvanadeLogWebPart extends BaseClientSideWebPart<IAvanadeLog
               <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
               <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
               <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
+              <p class="ms-font-l ms-fontColor-white">${escape(this.context.pageContext.web.absoluteUrl)}</p>
               <a href="https://aka.ms/spfx" class="${styles.button}">
                 <span class="${styles.label}">Learn more</span>
               </a>
             </div>
           </div>
         </div>
+        <div id="image">
+          <img src="${url}" alt='Log' />
+        </div>
       </div>`;
+    //OMG - different methods of the Log class
+    // review result Internet Explorer Developer tools
+    Log.info("Render", "Empezamos a renderizar el contenido");
+    Log.warn("Aviso", "Esta arrancando");
+    Log.error("Error", new Error("Es un error"));
+    Log.verbose("Verbose", "Logueo Verbose");
+
   }
 
   protected get dataVersion(): Version {
